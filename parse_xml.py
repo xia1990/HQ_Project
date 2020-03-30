796edb9778f569fec42b9511f61816b093bae520
unknown <102011123@huaqin.com>
2020-03-30 20:02:33 +0800

    add commit 2

--- parse_xml.py
+++ parse_xml.py
@@ -5,14 +5,34 @@ import xml.etree.ElementTree as ET
 import xlwt
 
 
-XML_FILE="new_mainfest.xml"
-tree=ET.parse("new_manifest.xml")
-root=tree.getroot()
+name_list=[]
+path_list=[]
+revision_list=[]
+title=['name','path','revision']
+wb=xlwt.Workbook()
+sheet1=wb.add_sheet("sheet1",cell_overwrite_ok=True)
+
+
+def set_style(name,height,bold=False):
+    style=xlwt.XFStyle()
+    pattern=xlwt.Pattern()
+    pattern.pattern=xlwt.Pattern.SOLID_PATTERN
+    #设置背景颜色
+    pattern.pattern_fore_colour=3
+    style.pattern=pattern
+    #设置边框(为实线)
+    borders=xlwt.Borders()
+    borders.left=xlwt.Borders.THIN
+    borders.right=xlwt.Borders.THIN
+    borders.top=xlwt.Borders.THIN
+    borders.bottom=xlwt.Borders.THIN
+    style.borders=borders
+    return style
 
 def get_project_info():
-	name_list=[]
-	path_list=[]
-	revision_list=[]
+	XML_FILE="new_mainfest.xml"
+	tree=ET.parse("new_manifest.xml")
+	root=tree.getroot()
 
 	for p in root.iter("project"):
 		project_name=p.get("name")
@@ -22,9 +42,11 @@ def get_project_info():
 		name_list.append(project_name)
 		path_list.append(project_path)
 		revision_list.append(project_revision)
-		print(len(name_list))
 
 def write_excel():
+	for i in range(len(title)):
+		sheet1.write(0,i,title[i],set_style('Times New Roman',220,True))
+
 	get_project_info()
 	for i in range(len(name_list)):
 		sheet1.write(i+1,0,name_list[i])
@@ -34,13 +56,7 @@ def write_excel():
 
 ##################################
 if __name__=="__main__":	
-	wb=xlwt.Workbook()
-	#创建一个excel表格
-	sheet1=wb.add_sheet("sheet1",cell_overwrite_ok=True)
-	sheet1.write(0,0,"name")
-	sheet1.write(0,1,"path")
-	sheet1.write(0,2,"revision")
-
+	write_excel()
 wb.save("manifest.xls")
 
 
